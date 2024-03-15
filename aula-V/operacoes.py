91127ceb258dfae87b2e936abf5f775e52fd97a8
FeliPython <felipejpa15@gmail.com>
2024-03-14 21:01:14 -0300

    arquivos da aula 5

---
+++ aula-V/operacoes.py
@@ -0,0 +1,27 @@
+class OperacoesMatematicas:
+    def soma(self, numero_1, numero_2):
+        return numero_1 + numero_2
+
+    def subtracao(self, numero_1, numero_2):
+        return numero_1 - numero_2
+
+    def divisao(self, numero_1, numero_2):
+        return numero_1 / numero_2
+
+    def multiplicacao(self, numero_1, numero_2):
+        return numero_1 * numero_2
+
+
+# def soma(numero_1, numero_2):
+#     return numero_1 + numero_2
+
+# def subtracao(numero_1, numero_2):
+#     return numero_1 - numero_2
+
+
+# def divisao(numero_1, numero_2):
+#     return numero_1 / numero_2
+
+
+# def multiplicacao(numero_1, numero_2):
+#     return numero_1 * numero_2
