6a34ee87089c8c1252ea20c563a20f805787b93c
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-03-16 17:32:13 -0300

    update exercicio1.py
    Teste do método set

--- desafio-semanal/exercicios/exercicio1.py
+++ desafio-semanal/exercicios/exercicio1.py
@@ -5,3 +5,10 @@
 # - Você pode utilizar conjuntos (sets) para remover duplicatas, já que conjuntos não permitem elementos duplicados.
 # - Caso queira, converta a lista para um conjunto. Em seguida, converta o conjunto de volta para uma lista.
 # - Imprima a lista resultante.
+
+lista_entrada = [1,2,6,3,7,4,2,1,6,2]
+
+lista_sem_duplicata = list(set(lista_entrada))
+
+print(f"Lista original: {lista_entrada}")
+print(f"Lista removendo as duplicatas: {lista_sem_duplicata}")
