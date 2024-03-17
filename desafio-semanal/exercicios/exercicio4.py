666940bf26b48d603bc74d6a57b7816713d75392
duartinh10 <matheusduarteengenharia@outlook.com>
2024-03-17 11:48:30 -0300

    add dever 5

--- desafio-semanal/exercicios/exercicio4.py
+++ desafio-semanal/exercicios/exercicio4.py
@@ -4,3 +4,19 @@
 # - Utilize um dicionário para armazenar o número de ocorrências de cada fruta.
 # - Passe pela lista e incremente o valor do dicionário para cada fruta encontrada.
 # - No final, imprima o dicionário.
+#Crie um programa que conte quantas vezes cada tipo de fruta aparece em uma lista.
+lista_frutas = []
+cont = 0
+tamanho_lista = int(input("Quantidade de frutas: "))
+for umaChave in range(tamanho_lista):
+    frutas = str(input("Nome de fruta: "))
+    lista_frutas.append(frutas)
+
+print(lista_frutas)
+
+for umaChave2 in lista_frutas:
+    contagem = lista_frutas.count(umaChave2)
+    if contagem > 1:
+        print(f"A fruta {umaChave2} aparece {contagem} vezes. ")
+
+
