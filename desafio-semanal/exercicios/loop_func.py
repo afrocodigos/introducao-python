0ae1951a2887c8af0f46f97a373dab3d46a00678
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-03-17 08:45:24 -0300

    fix nome variavel

--- desafio-semanal/exercicios/loop_func.py
+++ desafio-semanal/exercicios/loop_func.py
@@ -2,10 +2,10 @@ def loop_input():
     print("Insira cada valor da lista e pressione enter. Quando terminar de inserir itens na lista, digite 'Sair'")
 
     lista_usuario = []
-    item_entrada = input("Insira a sua entrada: ")
+    item_usuario = input("Insira a sua entrada: ")
 
-    while item_entrada != "Sair":
-        lista_usuario.append(item_entrada)
-        item_entrada = input("Insira a sua entrada: ")
+    while item_usuario != "Sair":
+        lista_usuario.append(item_usuario)
+        item_usuario = input("Insira a sua entrada: ")
     
     return lista_usuario
