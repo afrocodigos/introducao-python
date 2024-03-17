3597a182f2fb293a13c7471f7cffdd47137db29a
duartinh10 <matheusduarteengenharia@outlook.com>
2024-03-17 10:28:33 -0300

    add exercicio1

--- desafio-semanal/exercicios/exercicio1.py
+++ desafio-semanal/exercicios/exercicio1.py
@@ -5,3 +5,12 @@
 # - Você pode utilizar conjuntos (sets) para remover duplicatas, já que conjuntos não permitem elementos duplicados.
 # - Caso queira, converta a lista para um conjunto. Em seguida, converta o conjunto de volta para uma lista.
 # - Imprima a lista resultante.
+
+
+lista_nome1 = ["nome1","nome2","nome3"]
+lista_nome2 = ["nome1","nome4","nome5"]
+
+lista_juntas = set(lista_nome1+lista_nome2)#a estrutura set garante que os dados só iram ser mostrado uma vez
+lista_juntadas = list(lista_juntas)
+
+print(lista_juntadas)
