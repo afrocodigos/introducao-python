a734657c2330a39398277246f4fa8d5dc2314b12
Andreza Cristina <andrezadesenvolvedoraweb@gmail.com>
2024-03-17 20:32:12 -0300

    Update exercicio4.py
    Exercício concluído!

--- desafio-semanal/exercicios/exercicio4.py
+++ desafio-semanal/exercicios/exercicio4.py
@@ -4,3 +4,16 @@
 # - Utilize um dicionário para armazenar o número de ocorrências de cada fruta.
 # - Passe pela lista e incremente o valor do dicionário para cada fruta encontrada.
 # - No final, imprima o dicionário.
+
+frutas = ["limão", "banana", "uva", "limão", "laranja", "uva", "maçã", "abacaxi", "uva", "Pera", "limão", "uva", "melão","maçã", "laranja", "laranja", "uva", "abacaxi", "limão"]
+
+contagem_frutas = {}
+
+for fruta in frutas:
+    if fruta in contagem_frutas:
+        contagem_frutas[fruta] += 1
+    else:
+        contagem_frutas[fruta] = 1
+
+print("Contagem de frutas:", contagem_frutas)
+
