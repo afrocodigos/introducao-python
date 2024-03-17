353a1733c60db7017447016db8a468fd82ba7c02
duartinh10 <matheusduarteengenharia@outlook.com>
2024-03-17 10:47:58 -0300

    add dever 5

--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -4,3 +4,18 @@
 # - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
 # - Calcule o preço final aplicando o desconto percentual ao preço original.
 # - Imprima o preço final.
+from descontos import OperacoesProdutos
+
+instanciando_operacoes = OperacoesProdutos()
+
+produtos = int(input("Digite quantos produtos você quer: "))
+preco = float(input("Preço dos produtos: "))
+desconto = float(input("Desconto em porcentagem: "))
+
+total_a_pagar = instanciando_operacoes.valortotal(produtos,preco)
+pagar_com_desconto = instanciando_operacoes.descontoproduto(total_a_pagar,desconto)
+valor_com_desconto = instanciando_operacoes.valorfinal(total_a_pagar,pagar_com_desconto)
+
+print(f"Total a pagar sem Desconto: {total_a_pagar}")
+print(f"Valor do desconto: {pagar_com_desconto}")
+print(f"Total a pagar com Desconto: {valor_com_desconto}")
