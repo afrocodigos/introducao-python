dd5831f8dc166117f66bb1567ec18f3d21e3aad8
duartinh10 <matheusduarteengenharia@outlook.com>
2024-03-17 10:43:33 -0300

    add dever 3

--- desafio-semanal/exercicios/exercicio3.py
+++ desafio-semanal/exercicios/exercicio3.py
@@ -3,3 +3,10 @@
 # Dicas que podem ser seguidas ou não: 
 # - Use o método sort() para ordenar a lista em ordem alfabética.
 # - Certifique-se de usar o argumento key=str.lower para que a ordenação seja case-insensitive.
+lista_frutas = []
+tamanho_da_lista = int(input("Digite a quantidade de frutas"))
+for umaChave in range(tamanho_da_lista):
+    fruta = str(input("Digite a fruta: "))
+    lista_frutas.append(fruta)
+lista_frutas.sort()
+print(lista_frutas)
