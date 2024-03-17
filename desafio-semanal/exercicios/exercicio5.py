1c99a7363392aa19b6eb89c21a4b063d54c2aa52
Alfa Marine <alfamarine.ferreira@gmail.com>
2024-03-17 11:29:59 -0300

    add resolução exercício5.py

--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -1,6 +1,17 @@
+# AfroCódigos 2024
+# Autora: Alfa Marine
+
 # Crie um programa que calcule o preço final de um produto após aplicar um desconto percentual fornecido pelo usuário.
 
-# Dicas que podem ser seguidas ou não: 
-# - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
-# - Calcule o preço final aplicando o desconto percentual ao preço original.
-# - Imprima o preço final.
+# Cria função que calcula preço após desconto
+def calcula_preco_desconto(preco_original=0, desconto_percentual=1.0):
+    preco_desconto = preco_original*((100-desconto_percentual)/100)
+    return round(preco_desconto,2)
+
+# Testa a função de desconto
+precos_total_original = float(input("Insira o preço original: R$ "))
+desconto = int(input("Insira o valor de desconto(%): "))
+preco_total_desconto = calcula_preco_desconto(precos_total_original,desconto)
+
+print(f"Preço total original: R$ {precos_total_original}")
+print(f"Preço total após o desconto de {desconto}%: R$ {preco_total_desconto}")
