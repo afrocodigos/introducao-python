a62ac83d94014accafccb0282e388842ea53de14
Michelle Freitas <michelle.afreitas7@gmail.com>
2024-03-18 18:58:02 -0300

    Desafio semanal de Michelle Freitas

--- desafio-semanal/exercicios/exercicio4.py
+++ desafio-semanal/exercicios/exercicio4.py
@@ -1,6 +1,22 @@
 # Crie um programa que conte quantas vezes cada tipo de fruta aparece em uma lista.
 
-# Dicas que podem ser seguidas ou não: 
+# Dicas que podem ser seguidas ou não:
 # - Utilize um dicionário para armazenar o número de ocorrências de cada fruta.
 # - Passe pela lista e incremente o valor do dicionário para cada fruta encontrada.
 # - No final, imprima o dicionário.
+
+
+def count_repeat_items(list):
+    result = {}
+    for item in list:
+        result[item] = list.count(item)
+    show_result_count_items(result)
+
+
+def show_result_count_items(result):
+    for key, value in sorted(result.items()):
+        print(f'A fruta {key} aparece {value} vez(es).')
+
+
+fruit_list = ['melancia', 'cajá', 'morango', 'uva', 'jaca', 'uva', 'morango', 'uva']
+count_repeat_items(fruit_list)
--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -1,6 +1,15 @@
 # Crie um programa que calcule o preço final de um produto após aplicar um desconto percentual fornecido pelo usuário.
 
-# Dicas que podem ser seguidas ou não: 
+# Dicas que podem ser seguidas ou não:
 # - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
 # - Calcule o preço final aplicando o desconto percentual ao preço original.
 # - Imprima o preço final.
+
+
+def apply_discount(price, discount):
+    discount_percent =  1 - discount / 100
+    return f'O produto com valor R$ {price:.2f} aplicando desconto de {discount}% tem o valor final de R$ {(price * discount_percent):.2f}'
+
+product_price = 200
+product_discount = 15
+print(apply_discount(product_price, product_discount))
