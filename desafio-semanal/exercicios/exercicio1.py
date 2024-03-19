62227022879441b7d1746e74912993b1293033c7
Luã Moura <130193631+LhamCode@users.noreply.github.com>
2024-03-19 13:48:16 -0300

    Finalizado o exercício

--- desafio-semanal/exercicios/exercicio1.py
+++ desafio-semanal/exercicios/exercicio1.py
@@ -5,3 +5,33 @@
 # - Você pode utilizar conjuntos (sets) para remover duplicatas, já que conjuntos não permitem elementos duplicados.
 # - Caso queira, converta a lista para um conjunto. Em seguida, converta o conjunto de volta para uma lista.
 # - Imprima a lista resultante.
+
+import random
+# Criando uma lista com números sortidos e duplicatas usando o operador de multiplicação e shuffle
+
+def criar_lista_com_duplicatas(tamanho, duplicata):
+    numeros_ordenados = list(range(tamanho))
+    lista_com_duplicatas = [random.choice(numeros_ordenados) for _ in range(duplicata)] * tamanho
+    random.shuffle(lista_com_duplicatas)
+    return lista_com_duplicatas
+
+tamanho = 15
+duplicata = 5
+resultado = criar_lista_com_duplicatas(tamanho, duplicata)
+
+def remover_duplicatas(resultado):  
+    lista_sem_duplicatas = []
+    for elemento in resultado:
+        if elemento not in lista_sem_duplicatas:
+            lista_sem_duplicatas.append(elemento)
+    return lista_sem_duplicatas
+
+resultado_sem_duplicata = remover_duplicatas(resultado)
+
+print("Esta é a nossa lista!")
+print()
+print(resultado)
+print()
+print("Eliminamos as duplicatas =D")
+print()
+print(resultado_sem_duplicata)
