81f60bed951c39488a7cfb3495a7cbda5a39392d
Ana Paula Santos <santosilva.anapaula@gmail.com>
2024-03-21 16:06:22 -0300

    Feat: Solucao dos exercicios

--- desafio-semanal/exercicios/exercicio2.py
+++ desafio-semanal/exercicios/exercicio2.py
@@ -5,3 +5,13 @@
 # - Mantenha uma variável para armazenar o preço total.
 # - Adicione o preço de cada fruta ao preço total.
 # - Após inserir todos os preços, imprima o preço total.
+
+frutas = {"maça": 0, "pêra": 0, "laranja": 0, "melância": 0, "uva": 0}
+total_frutas = 0
+
+for fruta in frutas:
+    preco = float(input(f"Digite o preço da {fruta} : "))
+    frutas[fruta] = preco
+    total_frutas += preco
+
+print(f"O total é R${total_frutas}")
