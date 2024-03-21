b3984d81d794c933898c8e412bded7e48f5c9929
Vanilo Ferreira <vanilo.ferreira@fdte.io>
2024-03-20 22:47:48 -0300

    feat: Adicionar resolução do primeiro exercício

--- desafio-semanal/exercicios/exercicio1.py
+++ desafio-semanal/exercicios/exercicio1.py
@@ -5,3 +5,47 @@
 # - Você pode utilizar conjuntos (sets) para remover duplicatas, já que conjuntos não permitem elementos duplicados.
 # - Caso queira, converta a lista para um conjunto. Em seguida, converta o conjunto de volta para uma lista.
 # - Imprima a lista resultante.
+
+class CriarLista():
+  lista = []
+  
+  def adicionar_valor(self, valor):
+    self.lista.append(valor)
+
+while True:
+    print("==" * 50)
+    print("Escolha uma das opções abaixo:")
+    print("Digite 1 para inserir um valor na lista")
+    print("Digite 2 para sair do programa")
+    print("==" * 50)
+    print()
+    
+    entrada_do_usuario = int(input())
+    
+    if entrada_do_usuario == 1:
+      valor = input("Infome o valor: ")
+
+      CriarLista().adicionar_valor(valor)
+        
+      conjunto = set(CriarLista().lista)
+      tamanho_lista = len(CriarLista().lista)
+      nova_lista = list(conjunto)
+      tamanho_nova_lista = len(nova_lista)
+
+      print()
+      print("*" * 100)
+      print(f"Lista: {CriarLista().lista}")
+      print(f"Lista sem valores repetidos: {nova_lista}")
+      print()
+      print(f"A lista COMPLETA possui {tamanho_lista} elemento(s).")
+      print(f"A lista SEM dados repetidos possui {tamanho_nova_lista} elemento(s).")
+      print("*" * 100)
+      print()
+        
+    elif entrada_do_usuario == 2:
+      print("Encerrando o programa, volte sempre! ")
+      break
+    
+    else:
+        print("Opção inválida, por favor, digite uma opção correta!")
+        print()
