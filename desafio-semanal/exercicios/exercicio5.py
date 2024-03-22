858527fdaa07a4fe60058c3a86b9ad36182270bd
GabrielHenriquedev <ghss1@discente.ifpe.edu.br>
2024-03-22 20:41:47 -0300

    Solution of exercise five

--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -4,3 +4,15 @@
 # - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
 # - Calcule o preço final aplicando o desconto percentual ao preço original.
 # - Imprima o preço final.
+
+
+value_prod = float(input("Digite o valor do produto= "))
+percentage = float(input("Digite o valor do desconto= "))
+
+
+if percentage > value_prod:
+    print("O desconto não pode ser maior que o valor do produto.")
+else:
+    value_discount = value_prod * (percentage / 100)
+    value_final = value_prod - value_discount    
+    print("O preço final do produto após o desconto é:", value_final)
