46ff187fe981aec765e0a20e57cb95fc756d82ee
Tamir <amorimlds@gmail.com>
2024-03-22 08:09:41 -0300

    Resolução dos exercícios 1 a 5 - Desafio Semanal 24 mar | Python  | Afrocódigos

--- desafio-semanal/exercicios/exercicio4.py
+++ desafio-semanal/exercicios/exercicio4.py
@@ -1,5 +1,16 @@
 # Crie um programa que conte quantas vezes cada tipo de fruta aparece em uma lista.
 
+lista_frutas = ['maça', 'banana', 'uva', 'caja', 'banana', 'manga', 'acerola', 'maça', 'banana']
+lista_qtd_frutas = {} 
+
+for i in lista_frutas:
+    if i in lista_qtd_frutas:
+        lista_qtd_frutas[i] += 1        
+    else:
+        lista_qtd_frutas[i] = 1
+    
+print(lista_qtd_frutas)
+
 # Dicas que podem ser seguidas ou não: 
 # - Utilize um dicionário para armazenar o número de ocorrências de cada fruta.
 # - Passe pela lista e incremente o valor do dicionário para cada fruta encontrada.
--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -1,5 +1,15 @@
 # Crie um programa que calcule o preço final de um produto após aplicar um desconto percentual fornecido pelo usuário.
 
+valor_produto = float(input("Informe o valor do produto ->"))
+desconto_usuario = float(input("Informe o valor do desconto ->"))
+
+desconto_em_real = valor_produto * desconto_usuario / 100
+preco_final = valor_produto - desconto_em_real
+
+print("##" *30)
+print(f"Com um desconto de {desconto_usuario}% o valor final do produto é de {preco_final}")
+print("##" *30)
+
 # Dicas que podem ser seguidas ou não: 
 # - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
 # - Calcule o preço final aplicando o desconto percentual ao preço original.
