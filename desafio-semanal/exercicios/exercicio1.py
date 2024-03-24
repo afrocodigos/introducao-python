cf0ffe077cacd629154c4007cfdcaa0e7dc71ec9
GRAZIELE CARRASCOSA <grazielecarrascosa@hotmail.com>
2024-03-24 20:39:33 -0300

    exercicioSet com duas opções

--- desafio-semanal/exercicios/exercicio1.py
+++ desafio-semanal/exercicios/exercicio1.py
@@ -5,3 +5,11 @@
 # - Você pode utilizar conjuntos (sets) para remover duplicatas, já que conjuntos não permitem elementos duplicados.
 # - Caso queira, converta a lista para um conjunto. Em seguida, converta o conjunto de volta para uma lista.
 # - Imprima a lista resultante.
+
+lista = {"a", "gel", "cabelo", "a", "trunfo", }
+repeticao = set(lista)
+print(repeticao)
+
+lista2 = ["a", "colete", "implante", "a", "fresca", "lua", "lua"]
+listaSet = set(lista2)
+print(listaSet)
