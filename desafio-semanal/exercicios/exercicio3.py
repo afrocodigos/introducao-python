68eed742fc898feb879955f3742134000b15587a
michellekenia <keniamsoaress@gmail.com>
2024-03-24 19:00:13 -0300

    Exercicio 3

--- desafio-semanal/exercicios/exercicio3.py
+++ desafio-semanal/exercicios/exercicio3.py
@@ -3,3 +3,9 @@
 # Dicas que podem ser seguidas ou não: 
 # - Use o método sort() para ordenar a lista em ordem alfabética.
 # - Certifique-se de usar o argumento key=str.lower para que a ordenação seja case-insensitive.
+
+frutas = ["banana", "melancia", "maracuja", "abacaxi", "caju", "jabuticaba"]
+
+frutas.sort()
+
+print(frutas)
