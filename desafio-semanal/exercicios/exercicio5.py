46a17c6fa8a0a02a885b3b40b94efb969e79e56c
michellekenia <keniamsoaress@gmail.com>
2024-03-24 17:49:43 -0300

    Exercicio 5

--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -1,6 +1,13 @@
 # Crie um programa que calcule o preço final de um produto após aplicar um desconto percentual fornecido pelo usuário.
 
-# Dicas que podem ser seguidas ou não: 
-# - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
-# - Calcule o preço final aplicando o desconto percentual ao preço original.
-# - Imprima o preço final.
+def calcular_preco_final(preco_produto, desconto): 
+    preco_final = preco_produto - (preco_produto * (desconto/100))
+    return preco_final
+
+preco_produto = float(input("Digite o preço do produto: R$ "))
+desconto = float(input("Digite o valor do desconto percentual (%): "))
+
+preco_final = calcular_preco_final(preco_produto,desconto)
+
+print(f"O preço do produto após o desconto de {desconto}% é: {preco_final: .2f}R$")
+
