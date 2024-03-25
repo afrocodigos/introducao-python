7d4e124b26ea8e0b02fccf1a79bc13c482aab282
heloisebastos <heloisebastos28@hotmail.com>
2024-03-24 22:07:36 -0400

    sparkles:feat:aplica desconto sobre valor

--- desafio-semanal/exercicios/exercicio5.py
+++ desafio-semanal/exercicios/exercicio5.py
@@ -4,3 +4,18 @@
 # - Solicite ao usuário o preço original do produto e o desconto percentual ou armazene esses valores sem o input.
 # - Calcule o preço final aplicando o desconto percentual ao preço original.
 # - Imprima o preço final.
+
+def calcular_preco_final(preco_original, desconto_percentual):
+    desconto = (desconto_percentual / 100) * preco_original
+    preco_final = preco_original - desconto
+    return preco_final
+
+# Solicita ao usuário o preço original do produto e o desconto percentual
+preco_original = float(input("Digite o preço original do produto: "))
+desconto_percentual = float(input("Digite o desconto percentual (%): "))
+
+# Calcula o preço final
+preco_final = calcular_preco_final(preco_original, desconto_percentual)
+
+# Imprime o preço final
+print(f"O preço do produto com o desconto de {desconto_percentual} % é: R${preco_final:.2f}")
