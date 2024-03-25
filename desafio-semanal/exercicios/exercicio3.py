362dd8aea9c4d3327b7f807b94f3e90724f11ae5
Jessica <jessica-mss@hotmail.com>
2024-03-24 21:26:57 -0300

    Resolução exercícios do desafio semanal

--- desafio-semanal/exercicios/exercicio3.py
+++ desafio-semanal/exercicios/exercicio3.py
@@ -3,3 +3,21 @@
 # Dicas que podem ser seguidas ou não: 
 # - Use o método sort() para ordenar a lista em ordem alfabética.
 # - Certifique-se de usar o argumento key=str.lower para que a ordenação seja case-insensitive.
+
+def alphabetical_order(fruits_list):
+    fruits_list.sort(key=str.lower)
+    return (f"lista de frutas sem duplicados: {fruits_list}")
+
+# fruits_list = frutas = ["Banana", "Maçã", "Abacaxi", "Uva", "Pêssego", "Laranja"]
+fruits_list = []
+counter = 0
+
+while True:
+    user_input = input("Digite o nome da fruta ou sair para finalizar: ")
+    if user_input.lower() == "sair":
+        break
+    fruit = user_input
+    fruits_list.append(fruit)
+    counter += 1
+
+print (alphabetical_order(fruits_list))
--- desafio-semanal/exercicios/exercicio4.py
+++ desafio-semanal/exercicios/exercicio4.py
@@ -4,3 +4,28 @@
 # - Utilize um dicionário para armazenar o número de ocorrências de cada fruta.
 # - Passe pela lista e incremente o valor do dicionário para cada fruta encontrada.
 # - No final, imprima o dicionário.
+
+def count_fruits(fruits_list):
+    fruit_counts = {}
+
+    for fruit in fruits_list:
+        if fruit in fruit_counts:
+            fruit_counts[fruit] += 1
+        else:
+            fruit_counts[fruit] = 1
+
+    return fruit_counts
+
+fruits_list = []
+
+while True:
+    user_input = input("Digite o nome da fruta ou 'sair' para finalizar: ")
+    if user_input.lower() == "sair":
+        break
+    fruits_list.append(user_input)
+
+fruit_counts = count_fruits(fruits_list)
+
+print("Contagem de frutas:")
+for fruit, count in fruit_counts:
+    print(f"{fruit}: {count}")
